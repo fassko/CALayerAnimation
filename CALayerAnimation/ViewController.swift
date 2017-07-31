54b3d8f4642977811dd8a601d9aa40dae2c9267b
Kristaps Grinbergs <kristaps@qminderapp.com>
2017-07-31 16:02:36 +0300

    Removed old code

--- CALayerAnimation/ViewController.swift
+++ CALayerAnimation/ViewController.swift
@@ -30,32 +30,7 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    let path = CGMutablePath()
-    
-    
     shapeLayer = CAShapeLayer()
-    
-    
-    
-    
-//    for i in 0...10 {
-//      beziers.append(createBezierPath(point1: randomPointOnCircle(radius: 20.0, center: center), point2: point2, point3: point3))
-//    }
-    
-    
-    
-    
-//    bezierPath = createBezierPath(point1: randomPointOnCircle(radius: 10.0, center: center), point2: point2, point3: point3)
-//
-//    point1.x += 30
-//
-//    bezier2Path = createBezierPath(point1: point1, point2: point2, point3: point3)
-//
-//    point1.y += 30
-//
-//    bezier3Path = createBezierPath(point1: point1, point2: point2, point3: point3)
-    
-    
     
     let spiral = drawSpiral(arc: 10.0, separation: 3.0, numPoints: 100)
     
@@ -76,9 +51,7 @@ class ViewController: UIViewController {
     shapeLayer.lineWidth = 1.0
     shapeLayer.fillColor = UIColor.clear.cgColor
     
-    
     viewForLayer.layer.addSublayer(shapeLayer)
-
   }
   
   func createBezierPath(point1: CGPoint, point2: CGPoint, point3: CGPoint) -> UIBezierPath {
@@ -87,11 +60,6 @@ class ViewController: UIViewController {
     bezierPath.addLine(to: point2)
     bezierPath.addLine(to: point3)
     bezierPath.close()
-//    UIColor.black.setStroke()
-//    bezierPath.lineWidth = 1.5
-//    bezierPath.lineCapStyle = .round
-//    bezierPath.lineJoinStyle = .round
-//    bezierPath.stroke()
     
     return bezierPath
   }
@@ -110,74 +78,7 @@ class ViewController: UIViewController {
       animations.append(animation)
     }
     
-    
-//    for i in 0...2 {
-//      let animation = CABasicAnimation(keyPath: "path\(i)")
-//      animation.fromValue = i == 0 ? shapeLayer.path : beziers[i-1]
-//      animation.toValue = beziers[i]
-//      animation.duration = 1.0
-//      animation.repeatCount = .infinity
-//      animation.fillMode = kCAFillModeForwards
-//      animation.isRemovedOnCompletion = false
-//      animation.autoreverses = true
-//      animations.append(animation)
-//    }
-    
-//    let animation = CABasicAnimation(keyPath: "path1")
-//    animation.fromValue = shapeLayer.path
-//    animation.toValue = createBezierPath(point1: randomPointOnCircle(radius: 10.0, center: center), point2: point2, point3: point3).cgPath
-//    animation.duration = 1.0
-//    animation.repeatCount = .infinity
-//    animation.fillMode = kCAFillModeForwards
-//    animation.isRemovedOnCompletion = false
-//    animation.autoreverses = true
-//    animations.append(animation)
-//    
-//    let animation2 = CABasicAnimation(keyPath: "path2")
-//    animation2.fromValue = shapeLayer.path
-//    animation2.toValue = createBezierPath(point1: randomPointOnCircle(radius: 10.0, center: center), point2: point2, point3: point3).cgPath
-//    animation2.duration = 1.0
-//    animation2.repeatCount = .infinity
-//    animation2.fillMode = kCAFillModeForwards
-//    animation2.isRemovedOnCompletion = false
-//    animation2.autoreverses = true
-//    animations.append(animation2)
-    
-//    let animation = CABasicAnimation(keyPath: "path")
-//    animation.fromValue = beziers[0].cgPath
-//    animation.toValue = beziers[1].cgPath
-//    animation.duration = 1.0
-//    animation.beginTime = 0.0
-//    animation.repeatCount = .infinity
-//    animation.fillMode = kCAFillModeForwards
-//    animation.isRemovedOnCompletion = false
-//    animation.autoreverses = true
-    
-//    animations.append(animation)
-    
-//    let animation2 = CABasicAnimation(keyPath: "path")
-//    animation2.fromValue = beziers[1].cgPath
-//    animation2.toValue = beziers[2].cgPath
-//    animation2.duration = 1.0
-//    animation2.beginTime = animation.beginTime + animation.duration
-//    animation2.repeatCount = .infinity
-//    animation2.fillMode = kCAFillModeForwards
-//    animation2.isRemovedOnCompletion = false
-//    animation2.autoreverses = true
-//    animations.append(animation2)
-    
-//    let animation3 = CABasicAnimation(keyPath: "path")
-//    animation3.fromValue = beziers[2].cgPath
-//    animation3.toValue = beziers[3].cgPath
-//    animation3.duration = 1.0
-//    animation3.beginTime = animation2.beginTime + animation.duration
-//    animation3.repeatCount = .infinity
-//    animation3.fillMode = kCAFillModeForwards
-//    animation3.isRemovedOnCompletion = false
-//    animation3.autoreverses = true
-//    animations.append(animation3)
-    
-    group.animations = animations //[animation, animation2]
+    group.animations = animations
     group.repeatCount = .infinity
     group.duration = CFTimeInterval(animations.count)
     group.isRemovedOnCompletion = false
